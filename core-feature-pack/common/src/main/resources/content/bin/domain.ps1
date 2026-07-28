0f92ff69733b7a53b10bdba47cd7d2657a076c05
lvydra <lvydra@proton.me>
2026-07-28 11:10:51 +0200

    [JBEAP-32965][WFCORE-7576] standalone.sh version command uses standalone.conf JAVA_OPTS and can move and delete in use GC logs

--- core-feature-pack/common/src/main/resources/content/bin/domain.ps1
+++ core-feature-pack/common/src/main/resources/content/bin/domain.ps1
@@ -7,11 +7,14 @@
 $scripts = (Get-ChildItem $MyInvocation.MyCommand.Path).Directory.FullName;
 . $scripts'\common.ps1'
 Set-Item -Path env:JBOSS_LAUNCH_SCRIPT -Value "powershell"
-
-# Read an optional running configuration file
-$DOMAIN_CONF = "$scripts\domain.conf.ps1"
-$DOMAIN_CONF = Get-Env RUN_CONF $DOMAIN_CONF
-. $DOMAIN_CONF
+if ($global:VERSION) {
+    $PROCESS_CONTROLLER_JAVA_OPTS = '-Xmx16m'
+    $PRESERVE_JAVA_OPTS = $true
+} else {
+    # Read an optional running configuration file - skip for version/help commands
+    $DOMAIN_CONF = Get-Env RUN_CONF "$scripts\domain.conf.ps1"
+    . $DOMAIN_CONF
+}
 
 $JAVA_OPTS = Get-Java-Opts
 $SERVER_OPTS = Process-Script-Parameters -Params $ARGS
@@ -22,16 +25,18 @@ if ($SECMGR) {
     $MODULE_OPTS +="-secmgr";
 }
 
-if (-Not(Test-Path variable:DISABLE_JDK_SERIAL_FILTER)) {
-    $DISABLE_JDK_SERIAL_FILTER = Get-Env-Boolean DISABLE_JDK_SERIAL_FILTER $false
-}
-if (("$JAVA_OPTS $SERVER_OPTS $Env:JDK_JAVA_OPTIONS" -notlike "*-Djdk.serialFilter*") -and (!$DISABLE_JDK_SERIAL_FILTER)) {
-    if (-Not(Test-Path Env:JDK_SERIAL_FILTER)) {
-        $HOST_CONTROLLER_JAVA_OPTS += "`"@$scripts\jdk.serialFilter`""
-        $PROCESS_CONTROLLER_JAVA_OPTS += "`"@$scripts\jdk.serialFilter`""
-    } else {
-        $HOST_CONTROLLER_JAVA_OPTS += "-Djdk.serialFilter=""$Env:JDK_SERIAL_FILTER"""
-        $PROCESS_CONTROLLER_JAVA_OPTS += "-Djdk.serialFilter=""$Env:JDK_SERIAL_FILTER"""
+if (!$PRESERVE_JAVA_OPTS) {
+    if (-Not(Test-Path variable:DISABLE_JDK_SERIAL_FILTER)) {
+        $DISABLE_JDK_SERIAL_FILTER = Get-Env-Boolean DISABLE_JDK_SERIAL_FILTER $false
+    }
+    if (("$JAVA_OPTS $SERVER_OPTS $Env:JDK_JAVA_OPTIONS" -notlike "*-Djdk.serialFilter*") -and (!$DISABLE_JDK_SERIAL_FILTER)) {
+        if (-Not(Test-Path Env:JDK_SERIAL_FILTER)) {
+            $HOST_CONTROLLER_JAVA_OPTS += "`"@$scripts\jdk.serialFilter`""
+            $PROCESS_CONTROLLER_JAVA_OPTS += "`"@$scripts\jdk.serialFilter`""
+        } else {
+            $HOST_CONTROLLER_JAVA_OPTS += "-Djdk.serialFilter=""$Env:JDK_SERIAL_FILTER"""
+            $PROCESS_CONTROLLER_JAVA_OPTS += "-Djdk.serialFilter=""$Env:JDK_SERIAL_FILTER"""
+        }
     }
 }
 
